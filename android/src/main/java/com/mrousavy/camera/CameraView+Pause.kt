2da28df00f7e4538d571409a3f00ef5c683d91f7
datnx <datnx@gtech.ai>
2022-04-07 16:42:48 +0700

    update

--- android/src/main/java/com/mrousavy/camera/CameraView+Pause.kt
+++ android/src/main/java/com/mrousavy/camera/CameraView+Pause.kt
@@ -1,12 +1,18 @@
 package com.mrousavy.camera
 
 import android.annotation.SuppressLint
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 
 @SuppressLint("RestrictedApi")
 suspend fun CameraView.stopCamera() {
   Log.e(CameraView.TAG, "stopCamera")
-  cameraProvider?.unbindAll()
+  Handler(Looper.getMainLooper()).post(Runnable {
+    cameraProvider?.unbindAll()
+  })
+
+
 }
 
 @SuppressLint("RestrictedApi")
